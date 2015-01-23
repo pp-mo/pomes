b32ae94386dc4055adc2dad4e5fe748bf8774264
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2015-01-23 00:27:33 +0000

    Final-ish.

---
+++ all_go.java
@@ -0,0 +1,14 @@
+
+    // "All.go.rhythm"  : Paul Hertz
+
+    include everything.*;
+    void wonder(Universe u) {
+      while (ever || never) {
+        for (Poem i in u.now()) {
+          Word dust = u.speak(i);
+          for (Moment mote in dust) {
+            wonder(new Universe(mote));
+          }
+        }
+      }
+    }
